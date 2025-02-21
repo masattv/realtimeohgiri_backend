4df9f7271547c3551ed6f540321cf4b24870fb3b
masattv <k3absorb430@gmail.com>
2025-02-22 07:19:35 +0900

    Render fix

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
 from app import app, socketio
 import os
 
-# このファイルはgunicornによって直接使用される
-application = socketio.run(
-    app,
-    host='0.0.0.0',
-    port=int(os.environ.get("PORT", 5000))
-) 
+# gunicornで使用するapplication
+application = app
+
+if __name__ == "__main__":
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=int(os.environ.get("PORT", 5000))
+    ) 
